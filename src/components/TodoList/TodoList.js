e7f9770366cd3a751f21eb31c906519e6376a7ad
hillariee <hillonyechekwa@gmail.com>
2019-04-12 22:22:59 +0100

    initial commit

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = () => {
+        return(
+                <div>
+                        <input type="text" placeholder="Tasks"
+                </div>
+        )
+}
+
+export default TodoList
